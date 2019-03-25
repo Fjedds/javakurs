c522ab5053179d76963be5880f9881c09c14e299
student305 <student305@Kurs185.kurs.bouvet>
2019-03-25 15:05:29 +0100

    arrays

---
+++ Arrays/src/no/itfakultetet/java/App.java
@@ -0,0 +1,36 @@
+package no.itfakultetet.java;
+
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		int[] tall = { 12, 34, 2, 67 };
+
+
+		// Bla igjennom med for-løkke
+		
+
+		for (int i = 0; i < tall.length; i++) {
+			System.out.println(tall[i]);
+		}
+
+		System.out.println("------------");
+	
+		System.out.println(tall[3]);
+			
+		for (int i = 1; i <= 100; i+=10) {
+			System.out.println(i);
+			
+		}
+		
+		
+		
+		
+		} // Slutt på main
+		
+	} // slutt på klassen
+	
+	
+	
+	
